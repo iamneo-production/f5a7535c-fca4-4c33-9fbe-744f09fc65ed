55e2adaf7c72a80414fcb3a24688c1188fe623b4
varunsaigade <97600272+varunsaigade@users.noreply.github.com>
2022-03-31 19:20:34 +0530

    Update Homepage.jsx

--- reactapp/src/components/Homepage/Homepage.jsx
+++ reactapp/src/components/Homepage/Homepage.jsx
@@ -1,1 +1,25 @@
+import React, { Fragment } from "react";
+import { Outlet } from "react-router-dom";
 
+import AdminNavBar from "../Navbar/AdminNavBar";
+import UserNavbar from "../Navbar/UserNavBar";
+import { useAuthCxt } from "../Assets/auth-context";
+
+function HomePage(props) {
+  const authCxt = useAuthCxt();
+  const { userInfo, isLogged } = authCxt;
+  let navbar;
+  if (isLogged && userInfo.userType === "admin") {
+    navbar = <AdminNavBar />;
+  } else if (isLogged && userInfo.userType === "customer") {
+    navbar = <UserNavbar />;
+  }
+  return (
+    <Fragment>
+      {navbar}
+      <Outlet />
+    </Fragment>
+  );
+}
+
+export default HomePage;
